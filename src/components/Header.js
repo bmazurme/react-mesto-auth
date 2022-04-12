7704d01d7025d90ee65135b9f06712909c9867c7
Bogdan <b@bmazur.ru>
2022-04-13 00:01:33 +0300

    feat: header-menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import logo from '../images/logo.svg';
 import NavBar from './NavBar';
+import React from 'react';
 
 function Header(props) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  function handlerClick() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <header className="header">
+    <header className={`header ${isOpen ? 'header_opened' : ''}`}>
         <a href='/' 
            className="logo" 
            src={logo} alt="Логотип Место">
         </a>
-        <NavBar {...props}/>
+        <NavBar {...props} handlerClick={handlerClick} isOpen={isOpen}/>
     </header>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,11 @@ import { Link } from 'react-router-dom';
 import { Route, Switch } from "react-router-dom";
 
 function NavBar(props) {
+
   return(
     <>
-      <ul className='navbar'>
+      <ul onClick={props.handlerClick} 
+         className={`navbar ${props.isOpen ? 'navbar_opened' : ''}`}>
         <Switch>
           <Route exact path="/sign-in">
             <li className='navbar__item'>
@@ -44,8 +46,9 @@ function NavBar(props) {
 
         </Switch>
       </ul>
-      {/* <button className="navbar__btn"
-      /> */}
+
+      <button onClick={props.handlerClick} 
+              className={`navbar__btn ${props.isOpen ? 'navbar__btn_opened': ''}`} />
     </>
   );
 }
