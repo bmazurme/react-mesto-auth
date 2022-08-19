6c3ae166101a64351fb0775ee0bfabd51af822ea
Bogdan <b@bmazur.ru>
2022-08-19 18:05:17 +0300

    fix: props

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { Urls } from '../../utils/constants';
 
-// import { IProtectedRouteProps } from '../../interfaces/interfaces';
+import { IProtectedRouteProps } from '../../interfaces/interfaces';
 
-function ProtectedRoute({ loggedIn, children }: any) {
+function ProtectedRoute({ loggedIn, children }: IProtectedRouteProps) {
   if (!loggedIn) {
     return (
       <Navigate
