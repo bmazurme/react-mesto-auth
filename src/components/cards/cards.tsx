62a05c66260bae5d35d0c4d08b1bca92e44b00ba
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-04-26 21:38:03 +0300

    fix: low-case

---
+++ src/components/cards/cards.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { useErrorHandler } from 'react-error-boundary';
+
+import Card from '../card';
+import { ImagePopup } from '../popups';
+
+import { useChangeLikeMutation } from '../../store';
+
+export default function Cards({ user, cards }
+  : { user: User | null, cards: Card[] }) {
+  const errorHandler = useErrorHandler();
+  const [changeLike] = useChangeLikeMutation();
+  const [selectedCard, setSelectedCard] = useState<Card | null>(null);
+  const handleCardClick = (card: Card) => setSelectedCard(card);
+  const handleCloseAllPopups = () => setSelectedCard(null);
+
+  const handleCardLike = async (card: Card) => {
+    try {
+      await changeLike({
+        cardId: card._id,
+        value: card.likes.some((u) => u._id === user?._id),
+      });
+      handleCloseAllPopups();
+    } catch ({ status, data: { reason } }) {
+      errorHandler(new Error(`${status}: ${reason}`));
+    }
+  };
+
+  return (
+    <section className="cards">
+      {cards.map((card: Card) => (
+        <Card
+          key={card._id}
+          user={user}
+          card={card}
+          onCardClick={handleCardClick}
+          onCardLike={handleCardLike}
+        />
+      ))}
+      { selectedCard ? (<ImagePopup card={selectedCard} onClose={handleCloseAllPopups} />) : null}
+    </section>
+  );
+}
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">
+        &copy; 2022-2023 Mesto Russia [ntlstl] v.3
+      </p>
+    </footer>
+  );
+}
