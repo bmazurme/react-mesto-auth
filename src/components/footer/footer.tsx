e8cc1a1000106806abb80c3e453d52245f62791a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-29 22:24:10 +0300

    fix: footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import style from './footer.module.css';
+
 export default function Footer() {
   return (
-    <footer className="footer">
-      <p className="footer__copyright">
-        &copy; 2022-2023 Mesto Russia [ntlstl] v.3
+    <footer className={style.footer}>
+      <p className={style.copyright}>
+        &copy;
+        {' '}
+        {'2022-2023 Mesto Russia '}
+        <a className={style.link} href="https://ntlstl.dev/">[ntlstl]</a>
+        {' v.4'}
       </p>
     </footer>
   );
