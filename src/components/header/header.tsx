91117601e04d5fd392d2965ec0288a55e1045285
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-29 22:24:22 +0300

    fix: header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 import Logo from '../logo';
 import NavBar from '../nav-bar';
 
+import style from './header.module.css';
+
 export default function Header() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const handlerClick = () => setIsOpen(!isOpen);
-  const hedClass = (op: boolean) => `header${op ? ' header_opened' : ''}`;
+  const hedClass = (op: boolean) => `${style.header}${op ? ` ${style.opened}` : ''}`;
 
   return (
     <header className={hedClass(isOpen)}>
