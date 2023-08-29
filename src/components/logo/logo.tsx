b152dcccba5467e96fb8b8cc2adf74d0d6562958
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-29 22:23:40 +0300

    fix: logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from '../../images/logo.svg';
+import style from './logo.module.css';
 
 export default function Logo() {
   return (
-    <Link className="logo" to="/">
-      <img className="logo" src={logo} alt="Logo Mesto" />
+    <Link className={style.logo} to="/">
+      [mesto]
     </Link>
   );
 }
