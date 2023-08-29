b0da1941b006d7de6361d00827d21f5e36d10c0d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-29 22:26:24 +0300

    fix: signin-page

--- src/pages/signIn-page/signin-page.tsx
+++ src/pages/signIn-page/signin-page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import SignIn from './signin';
+import SigninLayout from '../../layouts/signin-layout';
 import Content from '../../components/content';
 import withUser from '../../hocs/with-user';
 
 function SignInPage() {
-  return (<Content children={<SignIn />} />);
+  return (<Content children={<SigninLayout />} />);
 }
 
 export default withUser(SignInPage, false);
