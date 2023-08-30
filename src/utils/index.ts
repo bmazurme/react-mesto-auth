b3b0c9e94feedd3e55adca15b26cf4dca2539a9c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-30 08:15:14 +0300

    fix: utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,3 +19,28 @@ export const isServer = !(
     && window.document
     && window.document.createElement
 );
+
+const MODAL_CONFIG = {
+  INITIAL: {
+    opacity: 0,
+    scale: 0.75,
+  },
+  ANIMATE: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      ease: 'easeOut',
+      duration: 0.35,
+    },
+  },
+  EXIT: {
+    opacity: 0,
+    scale: 0.75,
+    transition: {
+      ease: 'easeIn',
+      duration: 0.35,
+    },
+  },
+};
+
+export { MODAL_CONFIG };
