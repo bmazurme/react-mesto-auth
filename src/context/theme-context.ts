1a65b5b8335586c9cfb9ba7272a9c4c9c04ed071
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-30 08:30:26 +0300

    fix: theme-context

--- src/context/theme-context.ts
+++ src/context/theme-context.ts
@@ -1,11 +1,11 @@
 import React, { createContext } from 'react';
 
-type ThemeType = { style: string, setStyle: React.Dispatch<React.SetStateAction<string>> };
+type ThemeType = { isDark: string, setIsDark: React.Dispatch<React.SetStateAction<string>> };
 
 // Context lets us pass a value deep into the component tree
 // without explicitly threading it through every component.
 // Create a context for the current theme (with "light" as the default).
 // @ts-ignore
-const ThemeContext = createContext<ThemeType>({ style: false, setStyle: 'light' });
+const ThemeContext = createContext<ThemeType>({ isDark: false, setIsDark: 'light' });
 
 export default ThemeContext;
