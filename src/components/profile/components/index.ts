c87a759a5af8bfb642c7f4f87cf948760805d87e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-31 08:26:50 +0300

    fix: components

---
+++ src/components/profile/components/index.ts
@@ -0,0 +1,5 @@
+import AvatarButton from './avatar-button';
+import PlusButton from './plus-button';
+import ProfileButton from './profile-button';
+
+export { AvatarButton, PlusButton, ProfileButton };
