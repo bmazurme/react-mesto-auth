22b3975a09a9d40aa30c3bf305d420917bebe273
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-31 08:22:51 +0300

    fix: constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,3 +8,6 @@ export const Urls = {
 export const Regexp = {
   EMAIL: '[a-z0-9._%+-]+@[a-z0-9.-]+[\\.{0}][a-z]{2,3}$',
 };
+
+export const BASE_API_URL = 'https://mesto.nomoreparties.co/v1/cohort36';
+export const AUTHORIZATION = 'fcfa5c3a-c07d-49f3-a47d-0099ff285712';
