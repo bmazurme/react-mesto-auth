cad97f8401e6dec4b8feb7929a5263437f1d67cb
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-31 08:25:27 +0300

    main-layout

--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -3,12 +3,11 @@ import React from 'react';
 import Profile from '../../components/profile';
 import Cards from '../../components/cards';
 
-export default function MainLayout({ cards, user }
-  : { cards: Card[], user: User }) {
+export default function MainLayout() {
   return (
     <>
-      <Profile info={user} />
-      <Cards user={user} cards={cards} />
+      <Profile />
+      <Cards />
     </>
   );
 }
