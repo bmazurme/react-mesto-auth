36d5b4c0f3691d2a559d422f959fd1d5154de91a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-01 08:25:20 +0300

    fix: dev-config

---
+++ src/utils/dev-config.ts
@@ -0,0 +1,15 @@
+const DEV_JWT_SECRET = 'super-strong-secret';
+
+const OAUTH = {
+  YANDEX: {
+    CLIENT_ID: 'c709762dfe3e447999beb343da0bee9f',
+    CLIENT_SECRET: 'c1ff76a0170e4f49b9572665a233d901',
+  },
+  GITHUB: {
+    CLIENT_ID: 'd3a7b43f6b10622202cc',
+    CLIENT_SECRET: 'fcf674c4e0633d8b0bb8d755c78bfeed6e583636',
+  },
+};
+
+export default DEV_JWT_SECRET;
+export { OAUTH };
