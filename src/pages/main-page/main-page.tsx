536d690989c4d80b33ed68a5ab6c275811b7a5a9
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-01 08:20:48 +0300

    fix: main-page

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
-import { useErrorHandler } from 'react-error-boundary';
 
 import MainLayout from '../../layouts/main-layout';
 import Content from '../../components/content';
-import Preloader from '../../components/preloader';
 
 import withUser from '../../hocs/with-user';
-import { useGetCardsQuery, useGetUserMeQuery } from '../../store';
 
 function MainPage() {
-  const handleError = useErrorHandler();
-  const { error: cardsError, isLoading: isLoadingCards } = useGetCardsQuery();
-  const { error: userError, isLoading: isLoadingUser } = useGetUserMeQuery();
-
-  if (cardsError) {
-    handleError(cardsError);
-  }
-
-  if (userError) {
-    handleError(userError);
-  }
-
   return (
     <Content>
-      {isLoadingCards || isLoadingUser ? <Preloader /> : <MainLayout />}
+      <MainLayout />
     </Content>
   );
 }
 
-export default withUser(MainPage, true);
+export default withUser(MainPage, false);
