739c9487787d1edcafe231002155c42c233d4002
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-01 08:22:59 +0300

    fix: routes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import authRoute from './auth-route';
+import oauthRoute from './oauth-route';
+import cardRoute from './card-route';
 
-// import authMiddleware from '../middlewares/auth-middleware';
+import authMiddleware from '../middlewares/auth-middleware';
 
 const router = Router();
 
 router.use('/', authRoute);
+router.use('/', oauthRoute);
+router.use('/', authMiddleware, cardRoute);
 
 export default router;
