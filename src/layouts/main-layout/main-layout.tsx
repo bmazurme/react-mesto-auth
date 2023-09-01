aa8a7bcbd56317275a375ed6a9b34a6cf55ba70a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-01 08:19:26 +0300

    fix: main-layout

--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useErrorHandler } from 'react-error-boundary';
 
 import Profile from '../../components/profile';
 import Cards from '../../components/cards';
+import Preloader from '../../components/preloader';
+import useUser from '../../hooks/use-user';
+
+import { useGetCardsQuery, useGetUserMeMutation } from '../../store';
 
 export default function MainLayout() {
+  const data = useUser();
+  const handleError = useErrorHandler();
+  const { isError: cardsError, isLoading: isLoadingCards } = useGetCardsQuery();
+  const [getUserMe, { data: user, isLoading, isError }] = useGetUserMeMutation();
+
+  if (cardsError) {
+    handleError(cardsError);
+  }
+
+  if (isError) {
+    handleError(isError);
+  }
+
+  useEffect(() => {
+    getUserMe();
+  }, []);
+
   return (
     <>
-      <Profile />
-      <Cards />
+      {(data && user && !isLoading) && <Profile />}
+      {isLoadingCards ? <Preloader /> : <Cards />}
     </>
   );
 }
