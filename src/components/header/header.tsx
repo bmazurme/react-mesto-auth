8ecd188febe456f1c383e8f085915ad8bd531d20
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-01 08:13:13 +0300

    fix: header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,7 +7,7 @@ import NavBar from '../nav-bar';
 import style from './header.module.css';
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header className={classNames(style.header, { [style.opened]: isOpen })}>
